a02953e261a7066d7282933534fdfd260876df79
bsr4Ks <basaraksu03@gmail.com>
2023-12-09 00:01:25 +0300

    code navigation bar

--- ecommerce/shopify/views.py
+++ ecommerce/shopify/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request, "shopify/index.html")
+
+def login(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "shopify/login.html")
