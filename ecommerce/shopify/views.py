edb71e68b60342b167cda096e1c7941581635681
bsr4Ks <basaraksu03@gmail.com>
2023-12-10 19:45:28 +0300

    add a register form

--- ecommerce/shopify/views.py
+++ ecommerce/shopify/views.py
@@ -7,8 +7,17 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, "shopify/index.html")
 
+
+def register(request):
+    if request.method == "POST":
+        return render(request, "shopify/apology.html")
+    else:
+        return render(request, "shopify/register.html")
+
+
+
 def login(request):
     if request.method == "POST":
-        pass
+        return render(request, "shopify/apology.html")
     else:
         return render(request, "shopify/login.html")
